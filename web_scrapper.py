857b62ea11c00681c35604552b7abc2ee5eac2cb
Priyanshu Shukla <priyanshu.shukla@iedaas.com>
2021-09-18 20:24:23 +0530

    milestone completed get_synopsis add

--- web_scrapper.py
+++ web_scrapper.py
@@ -12,3 +12,19 @@ def top_movie(url,top_num):
     return movie_id_list
 
 
+
+def get_synopsis(movies_id):
+    base_url = "https://www.imdb.com/title/"
+    synopsis_list = []
+    for movie_id in movies_id:
+        request_ = requests.get(base_url+movie_id)
+        soup = bs4.BeautifulSoup(request_.text,'lxml')
+        synopsis = soup.select('.ipc-html-content.ipc-html-content--base')[1]
+        synopsis_list.append(synopsis.getText())
+    return synopsis_list
+
+movie_id_list = top_movie("https://www.imdb.com/chart/top/",1)
+synopsis_list = get_synopsis(movie_id_list)
+
+
+
