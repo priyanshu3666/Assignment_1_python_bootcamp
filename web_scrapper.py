cf975667b0d5ca9a8873f1a2b520e145f4030f86
Priyanshu Shukla <priyanshu.shukla@iedaas.com>
2021-09-18 20:36:37 +0530

    milestone5 completed fetching api added

--- web_scrapper.py
+++ web_scrapper.py
@@ -2,6 +2,8 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import requests,bs4
 import re
+
+#top_movie function  return the list of top n movies from IMDB website
 def top_movie(url,top_num):
     request_ = requests.get(url)
     soup =bs4.BeautifulSoup(request_.text,'lxml')
@@ -12,7 +14,7 @@ def top_movie(url,top_num):
     return movie_id_list
 
 
-
+#get_synopsis return the list of synopsis of movie_id from movies_id_list
 def get_synopsis(movies_id):
     base_url = "https://www.imdb.com/title/"
     synopsis_list = []
@@ -26,7 +28,7 @@ def get_synopsis(movies_id):
 movie_id_list = top_movie("https://www.imdb.com/chart/top/",1)
 synopsis_list = get_synopsis(movie_id_list)
 
-
+#this function return a list od bag_of_words
 def bag_of_words(synopsis_list):
     bag_of_words =[]
     all_stopwords = stopwords.words('english')
@@ -36,6 +38,7 @@ def bag_of_words(synopsis_list):
         bag_of_words.append([word for word in text_tokens if not word in all_stopwords])
     return bag_of_words
 
+# this create a dictionary of movie_id and synopsis
 def movie_dict(movie_id_list,synopsis_list):
     movies_dict = {}
     for movie_id in movie_id_list:
@@ -46,4 +49,14 @@ def movie_dict(movie_id_list,synopsis_list):
 movie_dict(movie_id_list,synopsis_list)
 
 
+Omdb_key = "64a6542a"
 
+# creating a api that return movie data
+def fetchting_movie_data(movie_id):
+    pattern = r"\D{2}\d{7}"
+    if re.compile(pattern).match(movie_id).group()==movie_id:
+        fetched_data = requests.get(f"http://www.omdbapi.com/?i={movie_id}&apikey={Omdb_key}")
+        synopsis_data = fetched_data.json()
+        return synopsis_data
+for movie_id  in movie_id_list:
+    fetchting_movie_data(str(movie_id))
