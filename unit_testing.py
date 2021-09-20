bd19123825a5caba3547ccf44f1b3de8375f5d5b
Priyanshu Shukla <44258359+priyanshu3666@users.noreply.github.com>
2021-09-21 00:45:22 +0530

    bag_of_words () function tested successful

--- unit_testing.py
+++ unit_testing.py
@@ -8,14 +8,15 @@ class Test_Web_Scraping(unittest.TestCase):
     def test_0_top_movie(self):
         self.assertRaises(TypeError,web_scrapper.top_movie("https://www.imdb.com/chart/top/","asds"))
         self.assertEqual(web_scrapper.top_movie("https://",5),None)
-        print("top_moive function tested successful")
+        print("\ntop_moive function tested successful")
     
     def test_1_get_synopsis(self):
         self.assertRaises(TypeError,web_scrapper.get_synopsis("mknj"))
-        print("get_synopsis function tested succesful")
+        print("\nget_synopsis function tested succesful")
         
     def test_3_bag_of_words(self):
-        pass
+        self.assertEqual(web_scrapper.bag_of_words('ram is shyam'),['ram shyam'])
+        print("\nbag_of_words() function tested successful")
         
             
 
