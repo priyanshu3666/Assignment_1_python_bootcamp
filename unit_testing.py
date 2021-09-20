fccc118574808793da1330e0910638b8bbf57ac8
Priyanshu Shukla <44258359+priyanshu3666@users.noreply.github.com>
2021-09-21 00:13:03 +0530

    get_synopsis() function tested

--- unit_testing.py
+++ unit_testing.py
@@ -5,13 +5,19 @@ import web_scrapper
 from unittest.mock import patch
 
 class Test_Web_Scraping(unittest.TestCase):
-    def test_top_movie(self):
+    def test_0_top_movie(self):
         self.assertRaises(TypeError,web_scrapper.top_movie("https://www.imdb.com/chart/top/","asds"))
         self.assertEqual(web_scrapper.top_movie("https://",5),None)
+        print("top_moive function tested successful")
     
-    def test_get_synopsis(self):
+    def test_1_get_synopsis(self):
+        self.assertRaises(TypeError,web_scrapper.get_synopsis("mknj"))
+        print("get_synopsis function tested succesful")
+        
+    def test_3_bag_of_words(self):
         pass
         
+            
 
 if __name__ == '__main__':
     unittest.main()
